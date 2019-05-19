c6e2611f08040e1ca55798c2f769fe66f55eb07f
rlau4 <p.lau.004@gmail.com>
2019-05-19 12:52:21 -0400

    finished product and added data for database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,10 +11,11 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    listInventory();
+    start();
 })
 
-function listInventory() {
+// List full inventory of available items for purchase.
+function start() {
     inquirer
         .prompt({
             name: "action",
@@ -26,14 +27,14 @@ function listInventory() {
                 "exit"
             ]
         })
-        .then(function(answer){
-            switch (answer.action){
+        .then(function (answer) {
+            switch (answer.action) {
                 case "See all products.":
                     allProducts();
                     break;
 
                 case "I know my product ID numbers and would like to place an order.":
-                    personalHygieneDepartmen();
+                    makeOrder();
                     break;
 
                 case "exit":
@@ -43,17 +44,70 @@ function listInventory() {
         });
 }
 
+// Shows all products customers can order
 function allProducts() {
     var query = "SELECT * FROM products";
-    connection.query(query, function(err, res){
-        for (var i = 0; i< res.length; i++){
+    connection.query(query, function (err, res) {
+        for (var i = 0; i < res.length; i++) {
             console.log(
                 "ID: " + res[i].id +
                 " || Product: " + res[i].product_name +
                 " || Department: " + res[i].department_name +
                 " || Price: $" + res[i].price
             );
-        
         }
+        makeOrder();
     })
-}
+};
+
+// Lets customer order a product via ID number
+function makeOrder() {
+    inquirer
+        .prompt([
+            {
+                name: "productIDinput",
+                type: "input",
+                message: "What is the ID of the item you would like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "numberToBeOrdered",
+                type: "input",
+                message: "How many would you like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function (answer) {
+            var query = "SELECT * FROM products WHERE ?"
+            connection.query(query, { id: answer.productIDinput }, function (err, res) {
+                console.log(res[0]);
+                var priceOfOrder = res[0].price * answer.numberToBeOrdered
+                //Checks if the number of products the customer wants to order is greater or less
+                // than the number in stock
+                if (answer.numberToBeOrdered <= res[0].stock_quantity) {
+
+                    // Lists total cost of items purchased and decrements stock in database
+                    var query = "UPDATE products SET stock_quantity = stock_quantity- ? WHERE id = ?";
+                    connection.query(query, [answer.numberToBeOrdered, answer.productIDinput],
+                        function (err, res) {
+                            console.log("Your total is $" + priceOfOrder + "!")
+                            start();
+                        })
+
+                } else {
+                    console.log("Insufficient quantity!")
+                    makeOrder();
+                };
+            });
+        })
+};
